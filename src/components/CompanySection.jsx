bf3cf3aae8d6c60cffea167383121dead7c6f24d
katwalreejan <Reejan.Katwal@metropolia.fi>
2025-05-20 22:33:04 +0300

    Added dynamic company logic

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -1,57 +1,27 @@
-import React from 'react'
+import React from "react";
+import { companies } from "../data/companies";
+import CompanyLogo from "./CompanyLogo";
 
 const CompanySection = () => {
   return (
-     <section className="clients section-padding">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-12 col-12">
-                <h3 className="text-center mb-5">Companies I've had worked</h3>
-              </div>
-
-              <div className="col-lg-2 col-4 ms-auto clients-item-height">
-                <img
-                  src="images/clients/cachet.svg"
-                  className="clients-image img-fluid"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-lg-2 col-4 clients-item-height">
-                <img
-                  src="images/clients/guitar-center.svg"
-                  className="clients-image img-fluid"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-lg-2 col-4 clients-item-height">
-                <img
-                  src="images/clients/tokico.svg"
-                  className="clients-image img-fluid"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-lg-2 col-4 clients-item-height">
-                <img
-                  src="images/clients/shopify.svg"
-                  className="clients-image img-fluid"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-lg-2 col-4 me-auto clients-item-height">
-                <img
-                  src="images/clients/profil-rejser.svg"
-                  className="clients-image img-fluid"
-                  alt=""
-                />
-              </div>
-            </div>
+    <section className="clients section-padding">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-12 col-12">
+            <h3 className="text-center mb-5">Companies I've had worked</h3>
           </div>
-        </section>
-  )
-}
 
-export default CompanySection
+          {companies.map((company) => (
+            <CompanyLogo
+              className="col-lg-2 col-4 ms-auto clients-item-height"
+              key={company.id}
+              src={company.src}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CompanySection;
---
+++ src/data/companies.ts
@@ -0,0 +1,7 @@
+export const companies = [
+  { id: 1, src: "images/clients/cachet.svg", alt: "Cachet" },
+  { id: 2, src: "images/clients/guitar-center.svg", alt: "Guitar Center" },
+  { id: 3, src: "images/clients/tokico.svg", alt: "Tokico" },
+  { id: 4, src: "images/clients/shopify.svg", alt: "Shopify" },
+  { id: 5, src: "images/clients/profil-rejser.svg", alt: "Profil Rejser" },
+];
