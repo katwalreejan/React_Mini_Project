5963cddb204ab72e4306d62f46c9df6713560b1c
sanjay-chalise <sanjchalise@gmail.com>
2025-05-20 12:13:30 +0300

    AboutSection component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import SliderSection from "./components/SliderSection";
-
+import AboutSection from "./components/AboutSection";
 function App() {
   return (
     <>
@@ -14,64 +14,7 @@ function App() {
 
         <Navbar />
         <SliderSection />
-
-        <section className="about section-padding" id="section_2">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-12">
-                <img
-                  src="images/couple-working-from-home-together-sofa.jpg"
-                  className="about-image img-fluid"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-lg-6 col-12 mt-5 mt-lg-0">
-                <div className="about-thumb">
-                  <div className="section-title-wrap d-flex justify-content-end align-items-center mb-4">
-                    <h2 className="text-white me-4 mb-0">My Story</h2>
-
-                    <img
-                      src="images/happy-bearded-young-man.jpg"
-                      className="avatar-image img-fluid"
-                      alt=""
-                    />
-                  </div>
-
-                  <h3 className="pt-2 mb-3">a little bit about Joshua</h3>
-
-                  <p>
-                    This one-page HTML portfolio is provided by{" "}
-                    <a href="https://templatemo.com" target="_blank">
-                      TemplateMo
-                    </a>
-                    . This layout is based on Bootstrap v5.1.3 CSS and JS
-                    libraries. Image credits go to{" "}
-                    <a href="https://unsplash.com" target="_blank">
-                      Unsplash
-                    </a>{" "}
-                    and{" "}
-                    <a href="https://freepik.com" target="_blank">
-                      FreePik
-                    </a>{" "}
-                    for images used in this page.
-                  </p>
-
-                  <p>
-                    You are allowed to use this template for your websites. You
-                    are not allowed to redistribute the template ZIP file on any
-                    other website. Please{" "}
-                    <a href="https://templatemo.com/contact" target="_blank">
-                      contact us
-                    </a>{" "}
-                    for more info.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <AboutSection/>
         <section className="featured section-padding">
           <div className="container">
             <div className="row">
